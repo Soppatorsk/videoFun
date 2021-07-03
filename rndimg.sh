0999b37c319c998da6834b0cb6b40e04c15d616e
Simon Karlsson <simon.kevlarsjal@gmail.com>
2021-07-03 12:10:12 +0200

    rndimg find

--- rndimg.sh
+++ rndimg.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-imgList=/tmp/imgrnd.txt
+imgList=/tmp/rndimg.txt
 DIR=`pwd -P`
-while getopts ":a" opt; do
-  case $opt in
-    a)
-    echo "export RNDIMG=\"\$RNDIMG`pwd -P`/snapsoup:\""
-		exit 1
-		;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-  esac
-done
 
-printenv RNDIMG | tr ':' '\n' | while IFS= read -r rndDir
+find /mnt/e/Video -name snapsoup | while read -r rndDir
 do
   for FILE in "$rndDir"/*.jpg
     do
@@ -27,6 +11,7 @@ do
 done
 cat $imgList | sort -R | echo "`head -n 1`"
 rm $imgList
-#current dir
-#wslview snapsoup/`ls snapsoup | sort -R | head -n 1`
+
+#find / -name snapsoup 2>/dev/null
+#^^find and all all snapsoup
 
